ce1db64605a53dc0b3385272c5999d60f5fdb2d3
Alon Mack <alonmack@gmail.com>
2015-10-28 22:25:36 +0300

    added spring security

--- src/main/java/com/alonmack/controller/FirstController.java
+++ src/main/java/com/alonmack/controller/FirstController.java
@@ -1,5 +1,8 @@
 package com.alonmack.controller;
 
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,10 +11,13 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class FirstController {
 
-    @RequestMapping("/")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
-        model.addAttribute("name", name);
-        return "index";
+    @RequestMapping(value={ "/login", "/index"})
+    public String home(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null && !(auth instanceof AnonymousAuthenticationToken)) {
+            model.addAttribute("name", name);
+            return "index";
+        }
+        return "login";
     }
-
 }
